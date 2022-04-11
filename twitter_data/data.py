e71b6008cf1dd1cfd98b4b8d3190463f1e4d0b78
shogoazgy <narita.shogo.bdlv@gmail.com>
2022-04-11 16:09:25 +0900

    fix

--- twitter_data/data.py
+++ twitter_data/data.py
@@ -6,7 +6,17 @@ import unicodedata
 import datetime
 import time
 import pytz
+import os
+import decimal
+import sys
+import re
+import unicodedata
+from igraph import *
+import leidenalg as la
+import igraph
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+mecab = MeCab.Tagger ()
 def word_separate(text, word_class=None):
     words = []
     text = unicodedata.normalize('NFKC', text)
@@ -25,12 +35,19 @@ def word_separate(text, word_class=None):
 
 
 def normalize(text):
-    text = re.sub(r'[1-9]', "", text)
+    text = re.sub(r'[0-9]', "", text)
     return re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "" , text)
+
+def get_tf_idf(text_list):
+    vectorizer = TfidfVectorizer(max_df=0.9)
+    vectorizer.fit(text_list)
+    return vectorizer
+
 
 def extract_users(path_origin, size, file_name='rt_users'):
     with open(file_name + '.txt', 'wt') as w:
-        for i in range(size):
+        flag = 0
+        for i in range(9, size):
             print(i)
             path = path_origin + str(i)
             with open(path) as f:
@@ -40,19 +57,75 @@ def extract_users(path_origin, size, file_name='rt_users'):
                         break
                     t = t.strip()
                     t = json.loads(t)
-                    rt_rted = str(t['user']['id_str']) + ',' + str(t['retweeted_status']['user']['id_str'])
-                    w.write(rt_rted + '\n')
+                    if flag:
+                        rt_rted = str(t['user']['id_str']) + ',' + str(t['retweeted_status']['user']['id_str'])
+                        w.write(rt_rted + '\n')
+                    else:
+                        if change_time(t['created_at']) == '2021-01-01':
+                            flag = 1
+                            rt_rted = str(t['user']['id_str']) + ',' + str(t['retweeted_status']['user']['id_str'])
+                            w.write(rt_rted + '\n')
+
 
 def change_time(created_at, only_date=True):
     st = time.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')        
-    utc_time = datetime.datetime(st.tm_year, st.tm_mon,st.tm_mday, \
-        st.tm_hour,st.tm_min,st.tm_sec, tzinfo=datetime.timezone.utc)   
+    utc_time = datetime.datetime(st.tm_year, st.tm_mon,st.tm_mday, st.tm_hour,st.tm_min,st.tm_sec, tzinfo=datetime.timezone.utc)   
     jst_time = utc_time.astimezone(pytz.timezone("Asia/Tokyo"))
     if only_date:
         str_time = jst_time.strftime("%Y-%m-%d")                    
     else:
         str_time = jst_time.strftime("%Y-%m-%d_%H:%M:%S")                    
     return str_time
+
+def test(path):
+    date = '2021-07-24'
+    i = 0
+    with open(path) as f:
+        while True:
+            t = f.readline()
+            if not t:
+                break
+            t = t.strip()
+            t = json.loads(t)
+            date_t = change_time(t['created_at'])
+            if date != date_t:
+                i += 1
+                print(t['created_at'])
+                #print(t)
+            if not t['user']['id_str'].isdecimal() or not t['retweeted_status']['user']['id_str']:
+                print(t)
+    print(i)
+
+def separate_days(path_origin, size, year_month, save_dir):
+    day = 1
+    w = open(os.path.join(save_dir, str(year_month) + '_' + str(day)), 'wt')
+    for i in range(size):
+        print(i)
+        path = path_origin + str(i)
+        with open(path) as f:
+            while True:
+                t = f.readline()
+                if not t:
+                    break
+                t = t.strip()
+                t = json.loads(t)
+                day_t = change_time(t['created_at'])
+                if int(day_t[8:]) == day:
+                    rt_rted = str(t['user']['id_str']) + ',' + str(t['retweeted_status']['user']['id_str'])
+                    w.write(rt_rted + '\n')
+                elif int(day_t[8:]) < day:
+                    x = open(os.path.join(save_dir, str(year_month) + '_' + str(int(day_t[8:]))), 'a')
+                    rt_rted = str(t['user']['id_str']) + ',' + str(t['retweeted_status']['user']['id_str'])
+                    x.write(rt_rted + '\n')
+                    x.close()
+                else:
+                    print(day_t)
+                    day += 1
+                    w.close()
+                    w = open(os.path.join(save_dir, str(year_month) + '_' + str(day)), 'wt')
+    w.close()
+
+
 
 def extract_date(path_origin, size, file_name='dates'):
     dates = collections.defaultdict(int)
@@ -73,12 +146,117 @@ def extract_date(path_origin, size, file_name='dates'):
             d = str(date) + ',' + str(dates[date])
             f.write(d + '\n')
         
- 
+def write_texts(paths, save_filename):
+    rt_user_list = []
+    with open(save_filename, 'wt') as w:
+        for path in paths:
+            i = 0
+            j = 0
+            print(path)
+            print(len(rt_user_list))
+            with open(path, 'r') as f:
+                while True:
+                    i += 1
+                    t = f.readline()
+                    if not t:
+                        break
+                    t = t.strip()
+                    t = json.loads(t)
+                    if 'retweeted_status' in t.keys():
+                        w.write(t['user']['id_str'] + ',' + t['retweeted_status']['user']['id_str'] + ',' + t['retweeted_status']['text'].replace('\n', '') + '\n')
+                    else:
+                        j += 1
+                print(i)
+                print(j)
 
+def walk_dir(path_origin, since=None, until=None):
+    paths = []
+    month = '02'
+    for pathname, dirnames, filenames in os.walk(path_origin):
+        for filename in sorted(filenames, key=str):
+            if filename[5:7] != month:
+                print(filename)
+                print('--reset--')
+                path = os.path.join(pathname, filename)
+                if filename[5:7] == '07':
+                    print(filename)
+                    write_texts(paths, save_filename=filename[:5] + month + '_tweets')
+                month = filename[5:7]
+                paths = []
+                paths.append(path)
+            else:
+                print(filename)
+                path = os.path.join(pathname, filename)
+                paths.append(path)
+    return paths
 
                 
 
 if __name__ == "__main__":
-    extract_users('/Users/shougo/Downloads/narita-mar/keyword-search.tweet.', 8, file_name='test')
+    #extract_users('/Users/shougo/Downloads/narita-mar/keyword-search.tweet.', 8, file_name='test')
     #extract_date('/Users/shougo/Downloads/narita-newyear/keyword-search.tweet.', 30, file_name='new_year_date')
-    #extract_users('/Users/shougo/Downloads/narita-newyear/keyword-search.tweet.', 30, file_name='rt_users_new_year')
+    #extract_users('/Users/shougo/Downloads/narita-newyear/keyword-search.tweet.', 30, file_name='rt_users_jan')
+    #separate_days('/Users/shougo/Downloads/narita-mar/keyword-search.tweet.', 8, '2021_3', '/Users/shougo/desktop/twitter/Twitter/twitter_data/2021_3')
+    #walk_dir('/Users/shougo/Downloads/narita2021')
+    #test('/Users/shougo/Downloads/data/str01_03/twitter/shohei/result/PFTqp4cu38skwf0xxkr_qp4cu10z9kwlp9c7r/2021-07-24.0.tweets')
+    g = g = Graph.Read_Ncol('/Users/shougo/Downloads/graphs_clusters/2020_04_clusters')
+    with open('/Users/shougo/Downloads/graphs_clusters/2020_04_membership') as f:
+      g.vs['cluster'] = [float(s.strip()) for s in f.readlines()]
+    user_sets = []
+    for i in range(20):
+        user_sets.append(set(g.vs.select(lambda vertex : vertex['cluster'] == i)['name']))
+    del g
+    words_all = []
+    words_list = []
+    for i in range(20):
+        words_list.append([])
+    with open('/Users/shougo/Desktop/twitter/Twitter/twitter_data/2020-04_tweets') as f:
+        i = 0
+        while(True):
+            t = f.readline()
+            if not t:
+                print(i)
+                break
+            t = t.strip()
+            i += 1
+            if i % 10 != 0:
+                continue
+            if i % 100000 == 0:
+                print(i)
+            t = t.split(',')
+            text = ''.join(t[2:])
+            words = word_separate(text, word_class=['名詞', '形容詞'])
+            words_all.append(words)
+            for j in range(20):
+                if t[0] in user_sets[j]:
+                    if t[1] in user_sets[j]:
+                        words_list[j].append(words)
+                        break
+                    else:
+                        break
+    vectorizer =  get_tf_idf(words_all)
+    with open('/Users/shougo/Desktop/twitter/Twitter/twitter_data/2020-04_tfidf', 'wt') as f:
+        for i in range(20):
+            texts = [' '.join(words_list[i])]
+            x = vectorizer.transform(texts)
+            feature_names = vectorizer.get_feature_names_out()
+            q = x.toarray().argsort()
+            q = q[0][::-1]
+            fre_words = []
+            for ind in q[0:40]:
+                fre_words.append(feature_names[ind])
+            words_str = ','.join(fre_words)
+            print('クラスタ' + str(i) + ': ' + words_str)
+            f.write(words_str + '\n')
+    """
+    for pathname, dirnames, filenames in os.walk('/Users/shougo/downloads/narita-newyear'):
+        for file in filenames:
+            path = os.path.join(pathname, file)
+            with open(path, 'r') as f:
+                while True:
+                    t = f.readline()
+                    if not t:
+                        break
+                    t = t.strip()
+                    t = json.loads(t)
+    """
--- twitter_data/graph.py
+++ twitter_data/graph.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 import pandas as pd
 import numpy as np
 from igraph import *
@@ -8,24 +9,17 @@ import requests
 import matplotlib.pyplot as plt
 import time
 import os
-def build_network(rt_users_path, save_filename='graph'):
-    with open(rt_users_path, 'r') as f:
-        rt_user_list = [s.strip() for s in f.readlines()]
-    print(len(rt_user_list))
-    c = collections.Counter(rt_user_list)
-    to_from_freq_list = []
-    del rt_user_list
-    for k in c.most_common():
-        #if k[1] <= 1:
-        #    break
-        to_from_freq = k[0].split(',')
-        #to_from_freq[0], to_from_freq[1] = to_from_freq[1], to_from_freq[0]
-        to_from_freq.append(k[1])
-        to_from_freq_list.append(to_from_freq)
-    g = Graph.TupleList(to_from_freq_list, weights=True, directed=True)
-    g.save(save_filename, format='gml')
-    summary(g)
-    return g
+import json
+import sys
+import subprocess
+import pickle
+from sklearn.linear_model import LinearRegression
+
+def count_lines(path):
+    cmd = "wc -l " + path
+    c = subprocess.check_output(cmd.split()).decode().split()[0]
+    return c
+
 
 def count_users(rt_users_path):
     with open(rt_users_path, 'r') as f:
@@ -110,30 +104,235 @@ def sort_max_print(subg, centrality, guest_token, len=10):
         ind = centrality.index(cen_sorted[i])
         print(id_to_screen_name(subg.vs[ind]['name'], guest_token=guest_token))
 
-def walk_dir(path_origin, since, until):
+def build_network(paths, save_filename):
+    rt_user_list = []
+    for path in paths:
+        print(path)
+        print(len(rt_user_list))
+        with open(path, 'r') as f:
+            while True:
+                t = f.readline()
+                if not t:
+                    break
+                t = t.strip()
+                t = json.loads(t)
+                try:
+                    rt_user_list.append(t['user']['id_str'] + ',' + t['retweeted_status']['user']['id_str'])
+                except:
+                    pass
+    c = collections.Counter(rt_user_list)
+    del rt_user_list
+    to_from_freq_list = []
+    for k in c.most_common():
+        to_from_freq = k[0].split(',')
+        to_from_freq.append(k[1])
+        to_from_freq_list.append(to_from_freq)
+    g = Graph.TupleList(to_from_freq_list, weights=True, directed=True)
+    g.save(save_filename, format='gml')
+    return g
+
+def walk_dir(path_origin, since=None, until=None):
     paths = []
-    flag = 0
+    x = []
+    y = []
+    month = '02'
     for pathname, dirnames, filenames in os.walk(path_origin):
         for filename in sorted(filenames, key=str):
-            if flag:
-                path = os.path.join(pathname, filename)
-                paths.append(path)
+            x.append(filename[:10])
+            path = os.path.join(pathname, filename)
+            y.append(count_lines(path))
+            if filename[5:7] != month:
                 print(filename)
-                if filename[:10] == until:
-                    flag = 0
-                    break
-            elif filename[:10] == since:
-                flag = 1
-                path = os.path.join(pathname, filename)
+                print('--reset--')
+                build_network(paths, filename[:5] + month)
+                month = filename[5:7]
+                paths = []
                 paths.append(path)
+            else:
                 print(filename)
-    return paths
+                paths.append(path)
+    return [x, y]
+
+def plot_tweet_count(path_origin):
+    day = '01'
+    c = 0
+    y_datas = []
+    x_datas = []
+    path = None
+    p_filename = None
+    for pathname, dirnames, filenames in os.walk(path_origin):
+        for filename in sorted(filenames, key=str):
+            if filename[8:10] != day:
+                print('--------')
+                y_datas.append(c)
+                x_datas.append(p_filename[:10])
+                c = 0
+                day = filename[8:10]
+            print(filename)
+            path = os.path.join(pathname, filename)
+            c += int(count_lines(path))
+            p_filename = filename
+    y_datas_average = []
+    y_datas_average.append(y_datas[0])
+    y_datas_average.append(y_datas[1])
+    y_datas_average.append(y_datas[2])
+    y_datas_average.append(y_datas[3])
+    for i in range(4, len(x_datas)):
+        y_datas_average.append((y_datas[i] + y_datas[i-1] + y_datas[i-2] + y_datas[i-3] + y_datas[i-4]) / 5)
+    plt.plot(x_datas, y_datas_average)
+    xt = [30 * i for i in range(15)]
+    plt.xticks(xt, rotation=90)
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
-    paths = walk_dir('/Users/shougo/Downloads/data/str01_03/twitter/shohei/result/PFTqp4cunv2kwvy8rsp_qp4cu7c1kwwyyveo', '2020-05-01', '2020-06-30')
+    rt_counts = []
+    for pathname, dirnames, filenames in os.walk('/Users/shougo/Downloads/narita2021'):
+        for filename in sorted(filenames, key=str):
+            rt_count = 0
+            path = os.path.join(pathname, filename)
+            with open(path, 'r') as f:
+                while True:
+                    t = f.readline()
+                    if not t:
+                        break
+                    t = t.strip()
+                    t = json.loads(t)
+                    if 'retweeted_status' in t.keys():
+                        rt_count += 1
+                print(rt_count)
+                rt_counts.append(rt_count)
+    with open("rt_counts_2021.pickle", mode="wb") as f:
+        pickle.dump(rt_counts, f)
 
     """
+    with open("x.pickle", mode="rb") as f:
+        x = pickle.load(f)
+    with open("y.pickle", mode="rb") as f:
+        y_t = pickle.load(f)
+    #y_c = pd.read_csv('/Users/shougo/Downloads/newly_confirmed_cases_daily.csv')['ALL'][16:714]
+    y_c = pd.read_csv('/Users/shougo/Downloads/newly_confirmed_cases_daily.csv')['ALL'][16:689]
+    x_i = [i for i in range(len(x))]
+    x_label = []
+    t = ['2/1\n2020', '4/1', '5/31', '7/30', '9/28', '11/27', '1/24', '3/25', '5/24', '7/23', '9/21', '11/20']
+    j = 0
+    for i in range(len(x) - 25):
+        if i % 60 == 0:
+            x_label.append(t[j])
+            #x_label.append(x[i])
+            j += 1
+        elif i == 333:
+            x_label.append('\n2021')
+        else:
+            x_label.append('')
+    plt.plot(x_i[:671], y_c[:671])
+    plt.grid(b=True, which="major", color="#ababab", linestyle="-", axis="y")
+    #plt.tick_params(labelbottom=False)
+    plt.xticks(x_i[:671], x_label[:671])
+    plt.xlabel('Date')
+    plt.ylabel('The number of new cases of infection')
+    
+
+    #plt.show()
+    plt.savefig('num_infection.pdf', bbox_inches="tight", pad_inches=0.05) # 'test.png'はpath+保存名です、figはplt.figureのオブジェクトです
+    """
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ln1 = ax1.plot(x_i[:671], y_t[:671], label='The number of tweets')
+
+    ax2 = ax1.twinx()
+    ln2 = ax2.plot(x_i[:671], y_c[:671], 'r', label='The number of new cases of infection')
+
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1+h2, l1+l2, loc='upper center')
+
+    ax1.set_xlabel('Data')
+    ax1.set_ylabel('The number of tweets')
+    ax1.grid(True)
+    ax2.set_ylabel('The number of new cases of infection')
+    plt.xticks(x_i[:671], x_label[:671])
+    #plt.show()
+    plt.savefig('num_tweets_infection.pdf', bbox_inches="tight", pad_inches=0.05) # 'test.png'はpath+保存名です、figはplt.figureのオブジェクトです
+    
+    start_dif = 29
+    start = 480
+    window = 160
+    y_c = y_c[start + start_dif:start + window + start_dif]
+    y_t = y_t[start + start_dif:start + window + start_dif]
+    x = x[start + start_dif:start + window + start_dif]
+
+    y_c_m = [[t] for t in y_c]
+    y_t_m = [[t] for t in y_t]
+    x_m = [[i] for i in range(len(y_t))]
+
+    model_t = LinearRegression()
+    model_t.fit(x_m, y_t_m)
+    model_c = LinearRegression()
+    model_c.fit(x_m, y_c_m)
+
+    
+    y_t_n = model_t.predict(x_m)
+    y_c_n = model_c.predict(x_m)
+    
+    y_t_avg = [y_t[i] - y_t_n[i][0] for i in range(len(x_m))]
+    y_c_avg = [y_c[i + start + 16 + start_dif] - y_c_n[i][0] for i in range(len(x_m))]
+
+    fig = plt.figure()
+    ax1 = fig.subplots()
+    ax2 = ax1.twinx()
+
+    ax1.plot(x, y_t_avg, label='tweet')
+    ax2.plot(x, y_c_avg, c="r", label='corona')
+    xt = [i for i in range(0, len(y_t), 45)]
+    plt.legend()
+    #plt.plot(x, y_t_avg)
+    #plt.plot(x, y_c_avg)
+    #print(y_c)
+    s1 = pd.Series(y_t)
+
+    s2 = y_c
+    corr = s1.corr(s2)
+    print(corr)
+
+    s1 = pd.Series(y_t_avg)
+    s2 = pd.Series(y_c_avg)
+    corr = s1.corr(s2)
+    print(corr)
+
+    plt.xticks(xt, rotation=90)
+    #plt.show()
+    
+    g = Graph.Read_GML('/Users/shougo/Desktop/twitter/Twitter/twitter_data/graphs/2020-04')
+    summary(g)
+    g.save('2020_04', format='ncol')
+    
+    paths = walk_dir('/Users/shougo/Downloads/narita2021/')
+    x, y = paths
+    with open("x2.pickle", mode="wb") as f:
+        pickle.dump(x, f)
+    with open("y2.pickle", mode="wb") as f:
+        pickle.dump(y, f)
+
+    
+    #plot_tweet_count('/Users/shougo/Downloads/data/str01_03/twitter/shohei/result/PFTqp4cunv2kwvy8rsp_qp4cu7c1kwwyyveo')
+    g = Graph.Read_GML('/Users/shougo/Desktop/twitter/Twitter/twitter_data/2020-03')
+    #vs = g.clusters(mode='weak')
+    summary(g)
+    p = clustering(g, resolution_parameter=0)
+    print(len(p))
+    print(p[1])
+    sub = subgraph(g, p[0])
+    summary(sub)
+    #p = la.find_partition(sub, la.ModularityVertexPartition, weights=sub.es['weight'], n_iterations=-1)
+    p = clustering(sub)
+    sub.vs['cluster'] = p.membership
+    sub.save('2021_03_clusters', format='gml')
+
+
     #g = Graph.Read_GML('2021_mar_1')
     g = build_network('rt_users_jan.txt')
     summary(g)
