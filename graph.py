319c4cfd312fd67a66f15c5e6540aae796c608c0
shogoazgy <narita.shogo.bdlv@gmail.com>
2022-05-19 18:18:32 +0900

    fix

--- graph.py
+++ graph.py
@@ -192,5 +192,7 @@ def calc_ration_over_communities(paths, g):
 if __name__ == "__main__":
     g = Graph.Read_GML('2020_04_quoted_clusters')
     print(g.vs[0]['cluster'])
-    p = la.ModularityVertexPartition(g, weights='weight', initial_membership=[str(i) for i in g.vs['cluster']])
+    in_membership = [str(i) for i in g.vs['cluster']]
+    print(type(in_membership[0]))
+    p = la.ModularityVertexPartition(g, weights='weight', initial_membership=in_membership)
     p.quality()
