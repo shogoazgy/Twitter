072a97edd59a15fea0c27518c0df03a95ef3ce3c
shogoazgy <narita.shogo.bdlv@gmail.com>
2022-06-24 10:01:36 +0900

    fix

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from asyncio import tasks
+import time
 from urllib.parse import urlparse
 import aiohttp
 import asyncio
@@ -10,67 +11,66 @@ import pickle
 from PIL import Image
 from io import BytesIO
 
-d = collections.defaultdict(int)
 
-all_img_count= 0
-img_found_count = 0
 i = 0
-async def fetch(url, session_get):
-    global img_found_count
+async def fetch(record, session_get):
     global i
-    global d
+    global not_found_urls
     i += 1
+    url, created_at = record.split(',')
     try:
         async with session_get(url) as res:
-            if i % 100 == 0:
+            if i % 1000 == 0:
                 print(i)
             if res.status == 200:
-                img_found_count += 1
                 id = urlparse(url).path[7:]
-                if id in d.keys():
-                    d[url] += 1
-                else:
-                    i = Image.open(BytesIO(res.content))
-                    i.save(id)
+                i = Image.open(BytesIO(res.content))
+                i.save('image/' + str(id))
             else:
-                #print(str(res.status) + ' : ' + str(url))
-                pass
+                not_found_urls.append(url)
     except:
         async with session_get(url) as res:
             print('あああああああああああああああああああああ')
             if res.status == 200:
-                img_found_count += 1
+                id = urlparse(url).path[7:]
+                i = Image.open(BytesIO(res.content))
+                i.save('image/' + str(id))
             else:
-                print('2 X')
+                not_found_urls.append(url)
 
-def scan_tweet(paths):
-    global all_img_count
-    urls = []
-    c_set = set()
-    for path in paths:
-        with open(path) as f:
-            while True:
-                tweet = f.readline().strip()
-                if not tweet:
-                    break
-                tweet = json.loads(tweet)
-                if 'extended_entities' in tweet.keys():
-                    for media in tweet['extended_entities']['media']:
-                        if media['type'] == 'photo':
-                            url = media['media_url_https']
-                            urls.append(url)
-                            all_img_count += 1
-                            c_set.add(url)
-                elif 'retweeted_status' in tweet.keys():
-                        if 'extended_entities' in tweet['retweeted_status'].keys():
-                            for media in tweet['retweeted_status']['extended_entities']['media']:
-                                if media['type'] == 'photo':
-                                    url = media['media_url_https']
-                                    urls.append(url)
-                                    all_img_count += 1
-                                    c_set.add(url)
-    print(len(c_set))
-    return urls
+def scan_tweet(path, url_set):
+    global record_list
+    global not_found_urls
+    records = []
+    with open(path) as f:
+        while True:
+            tweet = f.readline().strip()
+            if not tweet:
+                break
+            tweet = json.loads(tweet)
+            if 'extended_entities' in tweet.keys():
+                for media in tweet['extended_entities']['media']:
+                    if media['type'] == 'photo':
+                        url = media['media_url_https']
+                        if url in not_found_urls:
+                            pass
+                        else:
+                            records.append(str(url) + ',' + str(tweet['created_at']))
+                            if url not in url_set:
+                                url_set.add(url)
+                        
+            elif 'retweeted_status' in tweet.keys():
+                    if 'extended_entities' in tweet['retweeted_status'].keys():
+                        for media in tweet['retweeted_status']['extended_entities']['media']:
+                            if media['type'] == 'photo':
+                                url = media['media_url_https']
+                                if url in not_found_urls:
+                                    pass
+                                else:
+                                    records.append(str(url) + ',' + str(tweet['created_at']))
+                                    if url not in url_set:
+                                        url_set.add(url)
+    return [records, url_set]
 
 def walk_dir(path_origin):
     paths = []
@@ -81,24 +81,37 @@ def walk_dir(path_origin):
     return paths
 
 
-async def main(urls):
-    global d
+async def main(records):
     tasks = []
     to = aiohttp.ClientTimeout(total=10)
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=50)) as session:
         session_get = session.get
-        print(len(urls))
-        tasks = [fetch(url, session_get) for url in urls]
+        tasks = [fetch(record, session_get) for record in records]
         print('task start')
         await asyncio.wait(tasks)
-        print(all_img_count)
-        print(img_found_count)
-        with open('d.pickle', 'wb') as f:
-            pickle.dump(d, f)
 
 
 if __name__ == '__main__':
     paths = walk_dir('/home/narita/2020-covid-media-test/')
-    urls = scan_tweet(paths)
-    print('x')
-    #asyncio.get_event_loop().run_until_complete(main(urls))
+    record_list = []
+    with open('url_set.txt') as f:
+        url_set = set(f.read().splitlines())
+    with open('not_found_urls.txt') as f:
+        not_found_urls = f.read().splitlines()
+    
+    for path in paths:
+        print(path)
+        urls, url_set = scan_tweet(path, url_set)
+        asyncio.get_event_loop().run_until_complete(main(urls))
+        with open('record.txt', 'a') as w:
+            for record in record_list:
+                if record.split(',')[0] not in set(not_found_urls):
+                    w.write(record + '\n')
+        record_list = []
+        time.sleep(100)
+    
+    with open('url_set.txt', 'wt') as w:
+        w.write('\n'.join(list(url_set)))
+    with open('not_found_urls.txt', 'wt') as w:
+        w.write('\n'.join(not_found_urls))
+    
