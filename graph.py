b0d8152b2eafa6c8d5a7736e4fe8b8fb10b2f6af
shogoazgy <narita.shogo.bdlv@gmail.com>
2022-09-15 15:28:25 +0900

    fix

--- graph.py
+++ graph.py
@@ -107,26 +107,24 @@ def build_network(paths, save_filename, mode='quoted'):
     rt_user_list = []
     c = 0
     for path in paths:
-        #print(path)
-        print(path[-12:-10])
-        if path[-12:-10] == '06':
-            with open(path, 'r') as f:
-                while True:
-                    tweet = f.readline().strip()
-                    if not tweet:
-                        break
-                    tweet = json.loads(tweet)
-                    if mode == 'quoted':
-                        #if tweet['is_quote_status'] == True:
-                        try:
-                            rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['quoted_status']['user']['id_str'])
-                        except:
-                            c += 1
-                    elif mode == 'retweet':
-                        rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['retweeted_status']['user']['id_str'])
-                    elif mode == 'reply':
-                        if tweet['in_reply_to_user_id_str'] is not None:
-                            rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['in_reply_to_user_id_str'])
+        print(path)
+        with open(path, 'r') as f:
+            while True:
+                tweet = f.readline().strip()
+                if not tweet:
+                    break
+                tweet = json.loads(tweet)
+                if mode == 'quoted':
+                    #if tweet['is_quote_status'] == True:
+                    try:
+                        rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['quoted_status']['user']['id_str'])
+                    except:
+                        c += 1
+                elif mode == 'retweet':
+                    rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['retweeted_status']['user']['id_str'])
+                elif mode == 'reply':
+                    if tweet['in_reply_to_user_id_str'] is not None:
+                        rt_user_list.append(tweet['user']['id_str'] + ',' + tweet['in_reply_to_user_id_str'])
     print('無効なツイート: ' + str(c))
     c = collections.Counter(rt_user_list)
     del rt_user_list
